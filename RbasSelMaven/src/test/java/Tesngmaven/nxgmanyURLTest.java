ccb75b44456d456304283fce19ef25f2837cbd97
Raje_Siddhu <rajeswari.bashyam@nextguest.com>
2020-09-29 10:57:39 +0200

    RBAS newprogram

--- RbasSelMaven/src/test/java/Tesngmaven/nxgmanyURLTest.java
+++ RbasSelMaven/src/test/java/Tesngmaven/nxgmanyURLTest.java
@@ -1,13 +1,5 @@
 package Tesngmaven;
 
-import org.testng.annotations.Test;
-
-import Selenium.Xls_Reader;
-
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeClass;
-
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
@@ -15,10 +7,14 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import Selenium.Xls_Reader;
 
 public class nxgmanyURLTest {
 
--- RbasSelMaven/src/test/java/Tesngmaven/nxgsmoketest.java
+++ RbasSelMaven/src/test/java/Tesngmaven/nxgsmoketest.java
@@ -1,30 +1,20 @@
 package Tesngmaven;
 
-import org.testng.annotations.Test;
-
-import com.sun.xml.bind.v2.schemagen.xmlschema.List;
-
-import Selenium.Xls_Reader;
-
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeSuite;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeClass;
-
 import java.util.concurrent.TimeUnit;
-
-import javax.swing.text.html.HTMLDocument.Iterator;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import Selenium.Xls_Reader;
 
 public class nxgsmoketest {
 	 @BeforeSuite
